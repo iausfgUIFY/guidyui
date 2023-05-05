61e805a542d28f2ff7acc3145c9577a9fc479334
iausfgUIFY <132677192+iausfgUIFY@users.noreply.github.com>
2023-05-05 14:31:01 +0100

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,18 +1,12 @@
 input.onGesture(Gesture.Shake, function () {
     basic.showLeds(`
-        # . # # #
-        # . # . .
-        # # # # #
-        . . # . #
-        # # # . #
-        `)
-})
-basic.forever(function () {
-    basic.showLeds(`
         # . . . #
         . # . # .
         . . # . .
-        . # . # .
-        # . . . #
+        . . # . .
+        . . # . .
         `)
+})
+basic.forever(function () {
+	
 })
